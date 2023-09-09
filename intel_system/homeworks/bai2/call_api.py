9c7dcaa95a77052784275b8e263eef418a7453f5
anhnt <tien226anh@gmail.com>
2023-09-09 23:08:31 +0700

    update bai 2

--- intel_system/homeworks/bai2/call_api.py
+++ intel_system/homeworks/bai2/call_api.py
@@ -1,18 +1,19 @@
+# Run: python call_api.py
 import json
 import requests
+
+
 def predict_diabetes(BMI, Age, Glucose):
-  url = 'http://127.0.0.1:5000/diabetes/v1/predict'
-  data = {"BMI":BMI, "Age":Age, "Glucose":Glucose}
-  data_json = json.dumps(data)
-  headers = {'Content-type':'application/json'}
-  response = requests.post(url, data=data_json, headers=headers)
-  result = json.loads(response.text)
-  return result
+    url = "http://127.0.0.1:5000/diabetes/v1/predict"
+    data = {"BMI": BMI, "Age": Age, "Glucose": Glucose}
+    data_json = json.dumps(data)
+    headers = {"Content-type": "application/json"}
+    response = requests.post(url, data=data_json, headers=headers)
+    result = json.loads(response.text)
+    return result
+
 
 if __name__ == "__main__":
-  BMI = input('BMI?')
-  Age = input('Age?')
-  Glucose = input('Glucose?')
-  predictions = predict_diabetes(BMI,Age,Glucose)
-  print("Diabetic" if predictions["prediction"] == 1 else "Not Diabetic")
-  print("Confidence: " + predictions["confidence"] + "%")
+    predictions = predict_diabetes(30, 40, 100)
+    print("Diabetic" if predictions["prediction"] == 1 else "Not Diabetic")
+    print("Confidence: " + predictions["confidence"] + "%")
